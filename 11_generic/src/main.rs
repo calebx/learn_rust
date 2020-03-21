224b9264ca5cbaf453a75ab2d65ebbc214d9ba61
caleb.xiang <caleb.xiang@gmail.com>
2020-03-21 15:35:09 +0800

    generic struct & enum

--- 11_generic/src/main.rs
+++ 11_generic/src/main.rs
@@ -1,5 +1,12 @@
-fn largest<T: Copy + std::cmp::PartialOrd>(list: &[T]) -> T {
+use std::fmt::Display;
+use std::cmp::PartialOrd;
+
+// why here wre pick Copy instead of Clone:
+// as Copy Trait Type as i32, do not need be called explicitly
+// like we can do re-assignment here
+fn largest<T: Copy + PartialOrd>(list: &[T]) -> T {
     let mut largest = list[0];
+
     for &item in list.iter() {
         if item > largest {
             largest = item;
@@ -9,14 +16,6 @@ fn largest<T: Copy + std::cmp::PartialOrd>(list: &[T]) -> T {
     largest
 }
 
-fn longer_str<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
-}
-
 fn main() {
     let nums = [1, 2, 3, 2, 1];
     println!("largest nums is: {:?}", largest(&nums));
@@ -24,8 +23,77 @@ fn main() {
     let floats = [1.1, 1.2, 1.5, 0.0, 0.7];
     println!("largest floats is: {:?}", largest(&floats));
 
-    let str_1 = String::from("abcd");
-    let str_2 = "xyz!!";
-    let longer = longer_str(str_1.as_str(), str_2);
-    println!("longer one is {:?}", longer);
+    let chars = "hello".chars().collect::<Vec<_>>();
+    println!("largest char is: {}", largest(&chars));
+
+    generic_struct();
+
+    generic_enum();
+
+    generic_impl();
+}
+
+#[derive(Debug)]
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn generic_struct() {
+    let point = Point {
+        x: 3.0,
+        y: 4,
+    };
+
+    println!("point is {:?}", point);
+}
+
+#[derive(Debug)]
+enum Maybe<T> {
+    Just(T),
+    Nothing,
+}
+
+fn generic_enum() {
+    let m = Maybe::Just(3);
+    println!("m is {:?}", m);
+    let n: Maybe<u32> = Maybe::Nothing;
+    println!("n is {:?}", n);
+}
+
+#[derive(Debug)]
+struct Location<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T: Copy, U: Display + Copy> Location<T, U> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+
+    fn to_u(&self) -> Location<U, T> {
+        Location {
+            x: self.y,
+            y: self.x,
+        }
+    }
+
+    fn mixup<V, W: Copy>(&self, other: Location<V, W>) -> Location<T, W> {
+        Location {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn generic_impl() {
+    let l = Location { x: 10, y: 3.3 };
+    println!("l.x is {}", l.x());
+
+    let o = l.to_u();
+    println!("opposite l is {:?}", o);
+
+    let p = o.mixup(Location { x: "hel", y: "yeah" });
+    println!("mixup: {:?}", p);
 }
